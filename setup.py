d12229a5b116a2d7566aa9143b280de3e2163898
Urs Utzinger <utzinger@email.arizona.edu>
2026-02-24 19:37:00 -0700

    release: 1.0.3

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ def build_extensions():
 
 setup(
     name='uutzinger-pyimu',
-    version='1.0.0',
+    version='1.0.3',
     description=("Python implementation of AHRS with motion." ),
     url='https://github.com/uutzinger/pyIMU',
     author='Urs Utzinger',
